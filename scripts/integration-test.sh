fa662c14fc689561e1f69528d4584ccea59ad5e7
kensykora <ken.sykora@gmail.com>
2017-08-26 19:18:34 -0500

    Cleanup integration tests after run

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-  scripts/az-group-deploy.sh -g $AZ_RG_BUILD-$TRAVIS_BUILD_NUMBER -l $AZ_LOCATION -a src/arm
-fi
+set -ev
+scripts/az-group-deploy.sh -g $AZ_RG_BUILD-$TRAVIS_BUILD_NUMBER -l $AZ_LOCATION -a src/arm
