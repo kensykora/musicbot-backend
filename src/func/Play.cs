17cc6680cab042a3fd1b8073878540b747aeeba8
Ken Sykora <ken.sykora@gmail.com>
2017-09-03 12:53:10 -0500

    Play now requires POST

---
+++ src/func/Play.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace MusicBot.Functions
+{
+    public static class Play
+    {
+        [FunctionName("play")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "POST", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("Play Request");
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
