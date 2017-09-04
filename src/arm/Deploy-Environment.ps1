140d9a9252f57e7c62f2d5414aecffd85ed165ad
Ken Sykora <ken.sykora@gmail.com>
2017-09-04 18:14:27 -0500

    Attempt to work around git error stream issue

--- src/arm/Deploy-Environment.ps1
+++ src/arm/Deploy-Environment.ps1
@@ -7,8 +7,8 @@ param(
     [string] [Parameter(Mandatory=$true)]  $StorageAccountName
 )
 ./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName "$ResourceGroupName" -ResourceGroupLocation $ResourceGroupLocation -OtherParams @{ resourcePrefix = $ResourcePrefix; branch = $Branch; includeSourceControlDeployment = "no" } -UploadArtifacts -StorageAccountName $StorageAccountName
-git checkout .
-git checkout $Branch 2>&1
-git merge master
-git push origin $Branch 2>&1
+cmd /c git checkout . 2>&1
+cmd /c git checkout $Branch 2>&1
+cmd /c git merge master 2>&1
+cmd /c git push origin $Branch 2>&1
 ./src/arm/Update-VersionSetting.ps1 -ResourceGroupName "$ResourceGroupName" -AppServiceName "$ResourcePrefix-APP" -VersionNumber $Version
