6ae10874a6642d5454f4825ef5bc342e2bc1aacb
Ken Sykora <ken.sykora@gmail.com>
2017-09-04 17:26:13 -0500

    Fixed errors that were appearing in last build

--- src/arm/Deploy-Environment.ps1
+++ src/arm/Deploy-Environment.ps1
@@ -6,10 +6,10 @@ param(
     [string] [Parameter(Mandatory=$true)]  $Version,
     [string] [Parameter(Mandatory=$true)]  $StorageAccountName
 )
-$ErrorActionPreference = Stop
+$ErrorActionPreference = 'Stop'
 ./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName "$ResourceGroupName" -ResourceGroupLocation $ResourceGroupLocation -OtherParams @{ resourcePrefix = $ResourcePrefix; branch = $Branch; includeSourceControlDeployment = "no" } -UploadArtifacts -StorageAccountName $StorageAccountName
 git checkout .
-git checkout $Branch
+git checkout $Branch 2>&1
 git merge master
-git push origin $Branch
+git push origin $Branch 2>&1
 ./src/arm/Update-VersionSetting.ps1 -ResourceGroupName "$ResourceGroupName" -AppServiceName "$ResourcePrefix-APP" -VersionNumber $Version
