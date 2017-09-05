6c343fa4e120f525d273867467dbcfa05041886b
Ken Sykora <ken.sykora@gmail.com>
2017-09-04 22:06:23 -0500

    deploying by branch only

--- src/arm/deploy.bat
+++ src/arm/deploy.bat
@@ -1,5 +1,6 @@
-if "%APPVEYOR_REPO_TAG%" == "true" (
-  if "%APPVEYOR_REPO_TAG_NAME%" == "prod" (  
+$env: -eq $env: -and $env: -eq $null
+if "%APPVEYOR_REPO_BRANCH%" == "%PROD_BRANCH_NAME%" (
+  if "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" (  
     echo "Deploying to Production";
     git checkout .
     git checkout %PROD_BRANCH_NAME%
@@ -7,13 +8,15 @@ if "%APPVEYOR_REPO_TAG%" == "true" (
     git push origin %PROD_BRANCH_NAME%
   )
 ) else (
-  if "%APPVEYOR_REPO_TAG%" == "false" ( 
-    echo "Deploying to Dev";
-    git checkout .
-    git checkout %DEV_BRANCH_NAME%
-    git merge master
-    git push origin %DEV_BRANCH_NAME%
-  ) else ( 
-    echo "Non-Prod tagged commit, skipping deployment."
+  if "%APPVEYOR_REPO_BRANCH%" == "%DEV_BRANCH_NAME%" ( 
+    if "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" ( 
+      echo "Deploying to Dev";
+      git checkout .
+      git checkout %DEV_BRANCH_NAME%
+      git merge master
+      git push origin %DEV_BRANCH_NAME%
+    ) else ( 
+      echo "Non-Prod tagged commit, skipping deployment."
+    )
   )
 )
