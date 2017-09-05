72461c9618bb55ffcb64444fb595b524a92eff70
Ken Sykora <ken.sykora@gmail.com>
2017-09-04 20:04:50 -0500

    Attempt to fix script errors

--- src/arm/deploy.bat
+++ src/arm/deploy.bat
@@ -1,22 +1,22 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
   if "%APPVEYOR_REPO_TAG_NAME%" == "prod" (  
     echo "Deploying to Production";
-    powershell ./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName "%AZ_RG_PROD%" -ResourceGroupLocation "%AZ_LOCATION%" -OtherParams @{ resourcePrefix = "%AZ_RG_PROD%"; branch = "%PROD_BRANCH_NAME%"; includeSourceControlDeployment = "no" } -UploadArtifacts -StorageAccountName "%AZ_ARM_STORAGE_ACCOUNT_NAME%"
+    powershell "./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName '%AZ_RG_PROD%' -ResourceGroupLocation '%AZ_LOCATION%' -OtherParams @{ resourcePrefix = '%AZ_RG_PROD%'; branch = '%PROD_BRANCH_NAME%'; includeSourceControlDeployment = 'no' } -UploadArtifacts -StorageAccountName '%AZ_ARM_STORAGE_ACCOUNT_NAME%'"
     git checkout .;
     git checkout %PROD_BRANCH_NAME%;
     git merge master;
     git push origin %PROD_BRANCH_NAME%;
-    powershell ./src/arm/Update-VersionSetting.ps1 -ResourceGroupName "%AZ_RG_PROD%" -AppServiceName "%AZ_RG_PROD%-APP" -VersionNumber "%VERSION%%APPVEYOR_BUILD_NUMBER%"
+    powershell "./src/arm/Update-VersionSetting.ps1 -ResourceGroupName '%AZ_RG_PROD%' -AppServiceName '%AZ_RG_PROD%-APP' -VersionNumber '%VERSION%%APPVEYOR_BUILD_NUMBER%'
   )
 ) else (
   if "%APPVEYOR_REPO_TAG%" == "false" ( 
     echo "Deploying to Dev";
-    powershell ./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName "%AZ_RG_DEV%" -ResourceGroupLocation "%AZ_LOCATION%" -OtherParams @{ resourcePrefix = "%AZ_RG_DEV%"; branch = "%DEV_BRANCH_NAME%"; includeSourceControlDeployment = "no" } -UploadArtifacts -StorageAccountName "%AZ_ARM_STORAGE_ACCOUNT_NAME%";
+    powershell "./src/arm/Deploy-AzureResourceGroup.ps1 -ResourceGroupName '%AZ_RG_DEV%' -ResourceGroupLocation '%AZ_LOCATION%' -OtherParams @{ resourcePrefix = '%AZ_RG_DEV%'; branch = '%DEV_BRANCH_NAME%'; includeSourceControlDeployment = 'no' } -UploadArtifacts -StorageAccountName '%AZ_ARM_STORAGE_ACCOUNT_NAME%'
     git checkout .;
     git checkout %DEV_BRANCH_NAME%;
     git merge master;
     git push origin %DEV_BRANCH_NAME%;
-    powershell ./src/arm/Update-VersionSetting.ps1 -ResourceGroupName "%AZ_RG_DEV%" -AppServiceName "%AZ_RG_DEV%-APP" -VersionNumber "%VERSION%%APPVEYOR_BUILD_NUMBER%"
+    powershell "./src/arm/Update-VersionSetting.ps1 -ResourceGroupName '%AZ_RG_DEV%' -AppServiceName '%AZ_RG_DEV%-APP' -VersionNumber '%VERSION%%APPVEYOR_BUILD_NUMBER%'
   ) else ( 
     echo "Non-Prod tagged commit, skipping deployment.";
   )
