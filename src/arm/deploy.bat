64ae1f87bec777dd925d55c4ad6cbd6c8b6f0899
Ken Sykora <ken.sykora@gmail.com>
2017-09-04 22:32:59 -0500

    Removed unnecessary check

--- src/arm/deploy.bat
+++ src/arm/deploy.bat
@@ -1,21 +1,17 @@
 if "%APPVEYOR_REPO_BRANCH%" == "%PROD_BRANCH_NAME%" (
-  if "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" (  
-    echo "Deploying to Production";
-    git checkout .
-    git checkout %PROD_BRANCH_NAME%
-    git merge master
-    git push origin %PROD_BRANCH_NAME%
-  )
+  echo "Deploying to Production";
+  git checkout .
+  git checkout %PROD_BRANCH_NAME%
+  git merge master
+  git push origin %PROD_BRANCH_NAME%
 ) else (
   if "%APPVEYOR_REPO_BRANCH%" == "%DEV_BRANCH_NAME%" ( 
-    if "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" ( 
       echo "Deploying to Dev";
       git checkout .
       git checkout %DEV_BRANCH_NAME%
       git merge master
       git push origin %DEV_BRANCH_NAME%
-    ) else ( 
-      echo "Non-Prod tagged commit, skipping deployment."
-    )
+  ) else ( 
+    echo "Non-Prod tagged commit, skipping deployment."
   )
 )
