4435e0b142d088dddd887a573d8f21d962164a79
Ken Sykora <ken.sykora@gmail.com>
2017-09-10 14:48:41 -0500

    Added RegistrationCommand

---
+++ src/app/RegistrationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicBot.App
+{
+    public class RegistrationCommand
+    {
+        public RegistrationCommand(string registrationCode)
+        {
+            RegistrationCode = registrationCode;
+        }
+
+        public string RegistrationCode { get; }
+    }
+}
---
+++ test/app/RegistrationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MusicBot.App;
+
+using Xunit;
+
+namespace MusicBot.App.Test
+{
+    public class RegistrationTests
+    {
+        [Fact]
+        public void RegisterDevice_AcceptsRegistrationCode()
+        {
+            // arrange / act
+            var registrationCommand = new RegistrationCommand("ABC123");
+
+            // assert
+            Assert.Equal("ABC123", registrationCommand.RegistrationCode);
+        }
+    }
+}
